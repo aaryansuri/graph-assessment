328c594827c1c99a655b3a93f380919e6b0536e6
Aaryan Suri <aaryansuri0@gmail.com>
2024-08-01 00:24:33 +0530

    Add files via upload

--- demo/src/main/java/org/assessment/graph/service/MapGenerator.java
+++ demo/src/main/java/org/assessment/graph/service/MapGenerator.java
@@ -100,7 +100,7 @@ public class MapGenerator {
             Future<?> future = executor.submit(() -> start(city, cities, lanes, cityList, alone));
 
             try {
-                future.get(5, TimeUnit.SECONDS);
+                future.get(60, TimeUnit.SECONDS);
                 return;
             } catch (TimeoutException e) {
                 System.out.println("DFS Attempt " + (attempt + 1) + " timed out. Retrying...");
@@ -116,28 +116,36 @@ public class MapGenerator {
 
     private void start(int city, int cities, int lanes, List<City> cityList, boolean alone) {
 
-        boolean[][] linkEstablished = new boolean[cities][cities];
-        int[][] roadWeights = new int[cities][cities];
-        boolean[] visited = new boolean[cities];
-        List<Set<Integer>> hasLink = new ArrayList<>();
-        for(int i = 0; i < cities; i++) hasLink.add(new HashSet<>());
+//        boolean[][] linkEstablished = new boolean[cities][cities];
+        int[][] roadWeights = new int[cities + 2][cities + 2];
+//        boolean[] visited = new boolean[cities + 2];
+//        List<Set<Integer>> hasLink = new ArrayList<>();
+//        for(int i = 0; i <= cities; i++) hasLink.add(new HashSet<>());
 
-        visited[city] = true;
+        System.out.println(city);
+//        System.out.println(Arrays.toString(visited));
+        System.out.println(cities);
+//        visited[0] = true;
         if(!alone) {
-            hasLink.get(city).add(4);
+            roadWeights[city][city] = 4;
         }
 
 
         Set<Integer> travelled = new HashSet<>();
         travelled.add(city);
-        dfs(city, 0, linkEstablished, roadWeights, lanes, visited, hasLink, cities, new RandomCityGenerator(cities), travelled);
-        leader.add(cityList.get(city));
-        if(checkInvalidMove(roadWeights)) {
-            System.out.println("invalid graph");
+//        dfs(city, 0, linkEstablished, roadWeights, lanes, visited, hasLink, cities, new RandomCityGenerator(cities), travelled);
+        boolean[][][] cantMakeMemo = new boolean[cities + 2][lanes + 1][1 << (cities + 1)];
+        dfsDP(city, roadWeights, lanes, cities, cantMakeMemo, 1);
+        leader.add(cityList.get(0));
+
+        if(!alone) {
+            roadWeights[city][city] = 0;
         }
 
+        System.out.println("CC created " + city);
+
         for(int i = 0; i < cities; i++) {
-            for(int j = 0; j < roadWeights[0].length; j++) {
+            for(int j = 0; j < cities; j++) {
                 int weight = roadWeights[i][j];
                 if(weight != 0) {
                     roads.add(new Road(cityList.get(i), cityList.get(j), LaneType.getLane(weight)));
@@ -148,7 +156,7 @@ public class MapGenerator {
 
     }
 
-    private boolean dfs(
+    private boolean dfsDP(
         int city,
         int curr,
         boolean[][] linkEstablished,
@@ -193,9 +201,9 @@ public class MapGenerator {
                 visited[city] = true;
                 travelled.add(city);
                 if(visited[neighbour]) {
-                    cooked = dfs(neighbour, curr, linkEstablished, roadWeights, lanes - randomNextWeight, visited, hasLink, cities, random, travelled);
+                    cooked = dfsDP(neighbour, curr, linkEstablished, roadWeights, lanes - randomNextWeight, visited, hasLink, cities, random, travelled);
                 } else {
-                    cooked = dfs(neighbour, curr + 1, linkEstablished, roadWeights, lanes - randomNextWeight, visited, hasLink, cities, random, travelled);
+                    cooked = dfsDP(neighbour, curr + 1, linkEstablished, roadWeights, lanes - randomNextWeight, visited, hasLink, cities, random, travelled);
                 }
                 if(cooked) {
                     return true;
@@ -211,6 +219,50 @@ public class MapGenerator {
             }
 
         }
+
+        return false;
+    }
+
+    private boolean dfsDP(
+        int city,
+        int[][] roadWeights,
+        int lanes,
+        int cities,
+        boolean[][][] cantMake,
+        int mask
+    ) {
+
+        if((mask == (1 << cities) - 1) && lanes == 0) {
+//            Arrays.stream(roadWeights).map(Arrays::toString).forEach(System.out::println);
+            System.out.println(city);
+            return true;
+        }
+
+        if(lanes <= 0) return false;
+        if(lanes < cities - Integer.bitCount(mask) - 1) return false;
+        if(cantMake[city][lanes][mask]) {
+            return false;
+        }
+
+        for(int neighbour = 0; neighbour < cities; neighbour++) {
+            if(neighbour == city) continue;
+            // 1. travel neighbor if travelled or not
+            if((mask & (1 << neighbour)) == 0 || roadWeights[city][neighbour] == 0) {
+                for(int conn : possibleConnection(city, neighbour, roadWeights)) {
+                    roadWeights[city][neighbour] = conn;
+                    roadWeights[neighbour][city] = conn;
+                    int newMask = mask | (1 << neighbour);
+                    boolean makeConnectionAndGoNeigh = dfsDP(neighbour, roadWeights, lanes - conn, cities, cantMake, newMask);
+                    if(makeConnectionAndGoNeigh) return true;
+                    boolean makeConnectionAndStay = dfsDP(city, roadWeights, lanes - conn, cities, cantMake, newMask);
+                    if(makeConnectionAndStay) return true;
+                    roadWeights[city][neighbour] = 0;
+                    roadWeights[neighbour][city] = 0;
+                }
+            }
+        }
+
+        cantMake[city][lanes][mask] = true;
 
         return false;
     }
@@ -254,6 +306,68 @@ public class MapGenerator {
         Set<Integer> filteredCurrLinks = currLinks.stream().filter(n -> n != 0 && n != 4).collect(Collectors.toSet());
         Set<Integer> filteredNeighbourLinks = neighbourLinks.stream().filter(n -> n != 0 && n != 4).collect(Collectors.toSet());
 
+        return filteredCurrLinks.size() >= filteredNeighbourLinks.size() ? filteredCurrLinks : filteredNeighbourLinks;
+    }
+
+
+    // stream is slow
+    private static Set<Integer> possibleConnection(int mine, int neighbour, int[][] roadWeights) {
+        Set<Integer> neighbourLinks = new HashSet<>();
+        Set<Integer> currLinks = new HashSet<>();
+
+        // Collect non-zero road weights
+        for (int weight : roadWeights[neighbour]) {
+            if (weight != 0) neighbourLinks.add(weight);
+        }
+        for (int weight : roadWeights[mine]) {
+            if (weight != 0) currLinks.add(weight);
+        }
+
+        // Case when both sets are empty
+        if (currLinks.isEmpty() && neighbourLinks.isEmpty()) {
+            return Set.of(1, 2, 3);
+        }
+
+        // Case when neighbourLinks is empty and currLinks has only one element
+        if (neighbourLinks.isEmpty() && currLinks.size() == 1) {
+            int num = currLinks.iterator().next();
+            switch (num) {
+                case 4:
+                    return Set.of(3);
+                case 3:
+                    return Set.of(2, 3);
+                case 1:
+                    return Set.of(1, 2);
+                case 2:
+                    return Set.of(1, 2, 3);
+            }
+        }
+
+        // Check if any difference between elements is greater than 1
+        for (int curr : currLinks) {
+            for (int neigh : neighbourLinks) {
+                if (Math.abs(curr - neigh) > 1) {
+                    return Set.of();
+                }
+            }
+        }
+
+        // If sets are equal, return a new set with the same elements
+        if (currLinks.equals(neighbourLinks)) {
+            return new HashSet<>(currLinks);
+        }
+
+        // Filter out 0 and 4 from both sets
+        Set<Integer> filteredCurrLinks = new HashSet<>();
+        Set<Integer> filteredNeighbourLinks = new HashSet<>();
+        for (int n : currLinks) {
+            if (n != 0 && n != 4) filteredCurrLinks.add(n);
+        }
+        for (int n : neighbourLinks) {
+            if (n != 0 && n != 4) filteredNeighbourLinks.add(n);
+        }
+
+        // Return the larger set
         return filteredCurrLinks.size() >= filteredNeighbourLinks.size() ? filteredCurrLinks : filteredNeighbourLinks;
     }
 
